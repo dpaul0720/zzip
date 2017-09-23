bd4228b233bc440f7dc3c8d88c290eb429dbfc70
Paul Lee <dpaul0720@gmail.com>
2017-09-23 02:10:15 -0400

    Messed around with changing text and button events

--- app/src/main/java/com/example/paul/zzip/MainActivity.java
+++ app/src/main/java/com/example/paul/zzip/MainActivity.java
@@ -2,12 +2,57 @@ package com.example.paul.zzip;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import android.widget.Button;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private TextView xText, yText, zText;
+    private Sensor mySensor;
+    private SensorManager SM;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Create our Sensor Manager
+        SM = (SensorManager)getSystemService(SENSOR_SERVICE);
+
+        // Accelerometer Sensor
+        mySensor = SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        // Register sensor Listener
+        SM.registerListener(this, mySensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        // Assign TextView
+        xText = (TextView)findViewById(R.id.editText1);
+        yText = (TextView)findViewById(R.id.editText2);
+        zText = (TextView)findViewById(R.id.editText3);
     }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Not in use
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        xText.setText("X: " + event.values[0]);
+        yText.setText("Y: " + event.values[1]);
+        zText.setText("Z: " + event.values[2]);
+    }
+
+
+    public void testButton(View view){
+        //test button
+        Button button = (Button)findViewById(R.id.button);
+        button.setText("clicked");
+    }
+
 }
